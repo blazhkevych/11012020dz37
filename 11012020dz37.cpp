dc09e830a149f7ccd7d8cb647d844759a69ec19f
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:48:16 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz37.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* Пользователь 	вводит расстояние в милях.Перевести 	его в километры и морские мили.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Введите расстояние в милях : \n";
+	int a;
+	cin >> a;
+	cout << "км. = " << a * 1.609344 << ", морских миль = " << a * 0.868976242 << endl;
+	return 0;
+}
